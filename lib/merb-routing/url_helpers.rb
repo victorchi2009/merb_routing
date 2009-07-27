4b3ec50845eb86eded2b9cc19065b9b2655dee2f
squirrel <github@paybysquirrel.com>
2009-07-27 14:49:15 -0700

    Added support for options[:protocol] and options[:host] with defaults from the named route.

--- lib/merb-routing/url_helpers.rb
+++ lib/merb-routing/url_helpers.rb
@@ -8,22 +8,23 @@ module Merb::Router::UrlHelpers
 
   def method_missing_with_merb_routing(method_id, *args)
     if /^(formatted_)?(.*)(_url|_path)$/.match(method_id.to_s)
+      route_name = $2.to_sym
       options, args = args.last.is_a?(Hash) ? [args.pop, args] : [{}, args]
       
       if $1 == 'formatted_'
         options[:format] = args.pop
       end
-
+      
       host_prefix = ""
       if $3 == '_url' || options.delete(:only_path) == false
-        # logic copied from ActionController::UrlRewriter... no support for https (options[:protocol])
-        host_prefix << "http://"
+        route = Merb::Router.named_routes[route_name]
+        host_prefix << (options.delete(:protocol) || (route && route.conditions[:protocol]) || "http://")
         host_prefix << "#{CGI.escape(options.delete(:user))}:#{CGI.escape(options.delete(:password))}@" if options[:user] && options[:password]
-        host_prefix << (options.delete(:host) || ( request ? request.host_with_port : host ))
+        host_prefix << (options.delete(:host) || (route && route.conditions[:host]) || (request ? request.host_with_port : host))
         host_prefix << ":#{options.delete(:port)}" if options.key?(:port)
       end
       
-      return host_prefix + Merb::Router.url($2.to_sym, *(args << options << {}))
+      return host_prefix + Merb::Router.url(route_name, *(args << options << {}))
     else
       method_missing_without_merb_routing(method_id, *args)
     end
